237154b1d993a76ec731582876a11d7bfec51869
eyy4k <eolbergsveen@gmail.com>
2025-02-13 22:22:48 +0100

    b

--- script.js
+++ script.js
@@ -3,11 +3,21 @@
 
 let hasBlackJack = false
 let isAlive = false
+let dealersrunde = false;
 let message = "";
+
+let sum = 0;
+let dealersSum = 0
+let cards = [];
+let dealerCards = [];
+
 
 let messageEl = document.getElementById("messageEl")
 let sumEl = document.querySelector("#sumEl")
 let cardEl = document.getElementById("cardEl")
+
+let dealerKortEl = document.getElementById("dealersKort")
+let dealerSumEl = document.getElementById("dealersSum")
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
@@ -26,42 +36,95 @@ function getRandomCard() {
 function startGame() {
 
     isAlive = true;
+    hasBlackJack = false;
+    dealersrunde = false;
+
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
     cards = [firstCard, secondCard];
     sum = firstCard + secondCard;
+
+
+    let dealerFirstCard = getRandomCard();
+    let dealerSecondCard = getRandomCard();
+    dealerCards = [dealerFirstCard, dealerSecondCard];
+    dealerSum = dealerFirstCard + dealerSecondCard;
+
     renderGame()
+
 
 }
 
 function renderGame() {
     if (sum <= 20) {
         message = "Lyst På Et Til Kort?"
+        messageEl.style.color = "black"
     }
 
     else if (sum === 21) {
         message = "Blackjack!!! Du Vant";
+        messageEl.style.color = "green"
         hasBlackJack = true
     }
 
     else if (sum > 21) {
         message = "Beklager, Du Røyk!";
+        messageEl.style.color = "red"
         isAlive = false
     }
 
     messageEl.innerText = message;
 
-    sumEl.innerText = "sum: " + sum
+    sumEl.innerText = "Sum: " + sum
     cardEl.innerText = "Kort: "
-    for (let i = 0; i < cards.length; i++) { cardEl.textContent += cards[i] + " " }
+
+    for (let i = 0; i < cards.length; i++)
+         { cardEl.textContent += cards[i];
+            if (i < cards.length - 1) {
+                cardEl.textContent += ", " }
+
+        }
+
+    dealerKortEl.textContent = "Dealerens Kort: ???";
+    dealerSumEl.textContent = "Dealerens sum: ???";
 
 }
 
 function newCard() {
 
-    if (!isAlive || hasBlackJack) return;
+    if (!isAlive || hasBlackJack || dealersrunde) return;
     let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
+}
+
+function dealersTur() {
+
+    if (!isAlive || hasBlackJack) return;
+
+    dealersrunde = true
+
+    while (dealerSum < 17) {
+        let card = getRandomCard();
+        dealerCards.push(card);
+        dealerSum += card;
+    }
+
+    dealerKortEl.textContent = "Dealerens kort: ";
+
+    for (let i = 0; i < dealerCards.length; i++) {
+        dealerKortEl.textContent += dealerCards[i]; 
+    
+        if (i < dealerCards.length - 1) {
+            dealerKortEl.textContent += ", "; 
+        }
+    }
+
+    dealerKortEl.style.color = "black"
+
+    dealerSumEl.textContent = "Dealerens Sum: " + dealerSum;
+    dealerSumEl.style.color = "black"
+
+
 }
