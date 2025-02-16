29f2142d73b4e44c2442ef86bcdffcb294016952
eyy4k <eolbergsveen@gmail.com>
2025-02-16 15:07:13 +0100

    endret randomkort til at konge, j og dronning blir 10 og at ess blir enten 11 eller 1

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let sum = 0;
 let dealersSum = 0
 let cards = [];
 let dealerCards = [];
+let deck = [];
+
 
 let bheadEl = document.querySelector(".bhead")
 let messageEl = document.getElementById("messageEl")
@@ -22,19 +24,17 @@ let dealerSumEl = document.getElementById("dealersSum")
 let dealerScoreEl = document.querySelector("#dealerScore")
 let spillerScoreEl = document.querySelector("#spillerScore")
 
+// spiller og dealer får et random kort fra i til 11 //
+
 function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber > 10) {
-        return 10
-    }
 
-    else if (randomNumber === 1) {
-        return 11
-    }
+    if (deck.length === 0) {kortstokker()}
 
-    else { return randomNumber }
+    return deck.pop();
 
 }
+
+// starter spillet du får to kort og dealeren får et og det andre er skjult //
 
 function startGame() {
 
@@ -42,21 +42,28 @@ function startGame() {
     hasBlackJack = false;
     dealersrunde = false;
 
+    if (deck.length < 10) {kortstokker()}
+
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
+    firstCard = bestemEss(0, firstCard);
+    secondCard = bestemEss(firstCard, secondCard);
+
     cards = [firstCard, secondCard];
-    sum = firstCard + secondCard;
+    sum = cards.reduce((a, b) => a + b, 0);
 
 
     let dealerFirstCard = getRandomCard();
     let dealerSecondCard = getRandomCard();
     dealerCards = [dealerFirstCard, dealerSecondCard];
-    dealerSum = dealerFirstCard + dealerSecondCard;
+    dealerSum = dealerCards.reduce((a, b) => a + b, 0);
 
     renderGame()
 
 
 }
+
+// Nyttig funskjonalitet //
 
 function renderGame() {
     if (sum <= 20) {
@@ -96,18 +103,28 @@ function renderGame() {
 
 }
 
+// trekke enda flere kort //
+
 function newCard() {
 
     if (!isAlive || hasBlackJack || dealersrunde) return;
+
     let card = getRandomCard()
-    sum += card
+    card = bestemEss(cards, card);
+
+
+    
     cards.push(card)
+    sum = cards.reduce((a, b) => a + b, 0);
+
     renderGame();
 
     if (sum >= 21) {
         setTimeout(dealersTur, 1000);
     }
 }
+
+// dealerens Runde //
 
 function dealersTur() {
 
@@ -136,26 +153,29 @@ function dealersTur() {
     dealerSumEl.textContent = "Dealerens Sum: " + dealerSum;
     dealerSumEl.style.color = "black"
 
-bheadEl.innerHTML = "Trykk På Start Spillet For Ny Runde"
-bheadEl.style.fontSize = "40px"
+    bheadEl.innerHTML = "Trykk På Start Spillet For Ny Runde"
+    bheadEl.style.fontSize = "40px"
 
     vinner()
 
 }
 
+// Bestemmer Vinneren //
+
 let spillerScore = 0;
 let dealerScore = 0;
 
 function vinner() {
+
     if (!isAlive) return;
 
-    if (dealerSum > 21) { message = "Dealer Røyk! Du Vant!!!"; spillerScore++; messageEl.style.color = "green";}
+    if (dealerSum > 21) { message = "Dealer Røyk! Du Vant!!!"; spillerScore++; messageEl.style.color = "green"; }
 
     else if (dealerSum < sum && sum <= 21) { message = "Du Har Høyere Sum! Du Vant!!!"; spillerScore++; messageEl.style.color = "green"; }
 
     else if (dealerSum > sum && dealerSum <= 21) { message = "Du Har Lavere Sum! Du Tapte!!!"; dealerScore++; messageEl.style.color = "red"; }
 
-    else if (dealerSum > 21 && sum > 21) {message = "Begge Røyk! Ingen Vant!!!"; messageEl.style.color = "orange";}
+    else if (dealerSum > 21 && sum > 21) { message = "Begge Røyk! Ingen Vant!!!"; messageEl.style.color = "orange"; }
 
     else { message = "Uavgjort! Ingen Vant!!!"; messageEl.style.color = "blue"; }
 
@@ -164,6 +184,48 @@ function vinner() {
     spillerScoreEl.innerText = "Spillerens Score: " + spillerScore;
     dealerScoreEl.innerText = "Dealerens Score: " + dealerScore;
 
+}
+
+// Gjøre sånn de bruker fire kortstokker //
+
+function kortstokker() {
+
+    deck = [];
+
+    for (let i = 0; i < 4; i++)
+         { for (let j = 2; j <= 14; j++)
+             { 
+                let kortverdi = j
+                if (kortverdi > 10) kortverdi = 10;
+                if (kortverdi === 14) kortverdi = 11
+                
+                deck.push(kortverdi);
+             } } 
+
+     shuffleKort();
+
+}
+
+function shuffleKort() {
+
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+
+    }
+
+}
+
+// Ess kan både være 1 og 11 //
+
+function bestemEss(hånd, kort) {
+
+    if (kort === 11) 
+        
+        {let total = hånd + 11;
+        return total > 21 ? 1 : 11; }
+             
+    return kort;
 }
 
 // bytte mellom bakgrunner //
@@ -176,9 +238,9 @@ document.addEventListener("DOMContentLoaded", function () {
         "bilder/bakgrunn2.jpg",
         "bilder/bakgrunn4.jpg"]
 
-        const randomBakgrunn = Math.floor(Math.random() * bakgrunnsliste.length);
-        const valgtBakgrunnsbilde = bakgrunnsliste[randomBakgrunn]
+    const randomBakgrunn = Math.floor(Math.random() * bakgrunnsliste.length);
+    const valgtBakgrunnsbilde = bakgrunnsliste[randomBakgrunn]
 
-        document.body.style.backgroundImage = `url('${valgtBakgrunnsbilde}')`;
+    document.body.style.backgroundImage = `url('${valgtBakgrunnsbilde}')`;
 
 })
